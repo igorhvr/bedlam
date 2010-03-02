468162cc6e3c6bf80b66312b5f030a3e08c77e39
Igor Hjelmstrom Vinhas Ribeiro <igorhvr@iasylum.net>
2010-03-01 22:17:42 -0300

    Removed older irregex version.

--- iasylum/irregex/irregex-0.7.5/read-string.scm
+++
@@ -1,8 +0,0 @@
-
-(define (read-string . o)
-  (let ((in (if (pair? o) (car o) (current-input-port))))
-    (let lp ((ls '()))
-      (let ((c (read-char in)))
-        (if (eof-object? c)
-            (list->string (reverse ls))
-            (lp (cons c ls)))))))
