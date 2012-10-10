001923a34f188ca3b3f263794b8dfc28e2a44f26
Igor Hjelmstrom Vinhas Ribeiro <igorhvr@iasylum.net>
2012-10-10 00:55:38 -0300

    We now support an improved version of the email sending function using keyword arguments and useful default values for several of those.

--- iasylum/email.scm
+++ iasylum/email.scm
@@ -8,7 +8,7 @@
 ;;    activation-1.1.jar  commons-email-1.1.jar  mail.jar
 ;;    iasylum/jcode
 (module iasylum/email
-  (send-email)
+  (send-email send-email-k)
   
   ;;Test: (send-email "localhost" "igorhvr@iasylum.net" "igorhvr@iasylum.net" "igorhvr@iasylum.net" "igorhvr@iasylum.net" "assunto" "teste" )
 
@@ -31,6 +31,48 @@
          (senderemail ,(->jstring senderemail))
          (sendername ,(->jstring sendername))
          (subject ,(->jstring subject))
-         (messagetext ,(->jstring messagetext)))))))
-  
+         (messagetext ,(->jstring messagetext))))))
 
+  ;;Test: (send-email "localhost" "igorhvr@iasylum.net" "igorhvr@iasylum.net" "igorhvr@iasylum.net" "igorhvr@iasylum.net" "assunto" "teste" )
+
+
+                 
+  
+  (define send-email-k
+    (lambda* ((smtp-server: mailserver "localhost")
+         (recipient-email: recipientemail) (recipient-name: recipientname recipientemail)
+         (cc-email: ccemail recipientemail) (cc-name: ccname ccemail)
+         (sender-email: senderemail recipientemail) (sender-name: sendername senderemail)
+         (authentication-login: authentication-login "") (authentication-password: authentication-password #f) (use-ssl: use-ssl #f)
+         (subject: subject "") (message-text: messagetext "="))
+      (log-debug 'send-email mailserver recipientemail recipientname senderemail sendername subject messagetext)
+
+      (j 
+       "import org.apache.commons.mail.SimpleEmail;
+       SimpleEmail email = new SimpleEmail();
+       email.setHostName(mailserver);
+       email.addTo(recipientemail, recipientname);
+       email.addCc(ccemail, ccname);
+       if(!\"\".equals(authenticationlogin)) {
+           email.setAuthentication(authenticationlogin, authenticationpassword);
+       }
+       email.setSSL(usessl); 
+       email.setFrom(senderemail, sendername);
+       email.setSubject(subject);
+       email.setMsg(messagetext);
+       email.send();"
+       `((mailserver ,(->jstring mailserver))
+         (recipientemail ,(->jstring recipientemail))
+         (recipientname ,(->jstring recipientname))
+         (ccemail ,(->jstring ccemail))
+         (ccname ,(->jstring ccname))
+         (senderemail ,(->jstring senderemail))
+         (sendername ,(->jstring sendername))
+         (subject ,(->jstring subject))
+         (messagetext ,(->jstring messagetext))
+         (usessl ,(->jboolean use-ssl))
+         (authenticationlogin ,(->jstring authentication-login))
+         (authenticationpassword ,(->jstring authentication-password))
+         ))))
+    ;; Test (send-email-k 'recipientemail: "igorhvr@iasylum.net") 
+)
