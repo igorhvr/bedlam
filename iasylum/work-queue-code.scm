b042e2d02fdc080fac0ead0d1d23629f81728ca4
Igor Hjelmstrom Vinhas Ribeiro <igorhvr@iasylum.net>
2012-10-19 22:01:38 -0300

    We now support blocking queues and workers that never stop working in our work queue processing module. We also removed the compiled version of this module.

--- iasylum/work-queue-code.scm
+++ iasylum/work-queue-code.scm
@@ -2,30 +2,51 @@
 
 
 (define make-queue
-  (lambda ()
-    (let ((inner-queue (j "new java.util.concurrent.ConcurrentLinkedQueue();"))
+  (lambda* ((capacity #f))
+    (let ((inner-queue
+           (if capacity
+               (j "new java.util.concurrent.LinkedBlockingQueue(capacity);" `((capacity ,(->jint capacity))))
+               ;;(j "new java.util.concurrent.ConcurrentLinkedQueue();"))
+               (j "new java.util.concurrent.LinkedBlockingQueue();"))
+           )
           (paused #f))
       (letrec ((new-function (match-lambda*
                               [('add v)
-                               (j "q.add(v);"  `((q ,inner-queue) (v ,(java-wrap v))))]
+                               (if capacity
+                                   (j "q.put(v);"  `((q ,inner-queue) (v ,(java-wrap v))))
+                                   (j "q.add(v);"  `((q ,inner-queue) (v ,(java-wrap v)))))
+                               ]
+                              [('put v)
+                               (if capacity
+                                   (j "q.put(v);"  `((q ,inner-queue) (v ,(java-wrap v))))
+                                   (j "q.add(v);"  `((q ,inner-queue) (v ,(java-wrap v)))))
+                               ]
                               [('clear)
                                (j "q.clear();"  `((q ,inner-queue)))]
                               [('pause)
                                (set! paused #t) (void)]
                               [('resume)
                                (set! paused #f) (void)]
-                              [('pool return-when-empty)
+                              [('take return-when-paused)
+                               (if (not paused)
+                                   (let ((result (j "q.take();"  `((q ,inner-queue)))))
+                                     (java-unwrap result))
+                                   return-when-paused)
+                               ]
+                              [('take)
+                               (new-function 'take 'paused)]
+                              [('poll return-when-empty)
                                (if (not paused)
                                    (let ((result (j "q.poll();"  `((q ,inner-queue)))))
                                      (if (java-null? result) return-when-empty (java-unwrap result)))
                                    return-when-empty)
                                ]
-                              [('pool)
-                               (new-function 'pool 'empty)])))
+                              [('poll)
+                               (new-function 'poll 'empty)])))
         new-function))))
 
 (define (process-all-work proc queue)
-  (let ((v (queue 'pool)))
+  (let ((v (queue 'poll)))
     (if (not (eqv? v 'empty)) (begin (proc v) (process-all-work proc queue)))))
 
 (define get-next-worker-n
@@ -40,20 +61,25 @@
 
 (define (now) (->string (j "new java.util.Date().toString();")))
 
-(define (start-worker processor work-queue)
-  (thread/spawn
-   (lambda ()
-     (with-failure-continuation
-      (lambda (err cont)
-	(d "\nError " err " at " cont "\n"))
-      (lambda ()
-	(let ((n (get-next-worker-n)))
-	  (d "\nStarting worker [w" n "]...\n")
-	  (process-all-work
-	   (lambda (v)
-	     (define (now) (->string (j "new java.util.Date().toString();")))
-	     (d "\n   [w" n "] starting  work unit: " v " (now: " (now) ")\n")
-	     (let ((timings (time (processor v)))) (d "\n   [w" n "] completed work unit: " v " in " (cdr timings) "\n"))
-	     )       
-	   work-queue)
-	  (d "\nStopping worker [w" n "] - nothing else to do.\n")))))))
+(define start-worker
+  (lambda* (processor work-queue (continue-forever #f))
+      (thread/spawn
+       (lambda ()
+         (with-failure-continuation
+          (lambda (err cont)
+            (d "\nError " err " at " cont "\n"))
+          (lambda ()
+            (let ((n (get-next-worker-n)))
+              (d "\nStarting worker [w" n "]...\n")
+              (let r ()
+                (process-all-work
+                 (lambda (v)
+                   (define (now) (->string (j "new java.util.Date().toString();")))
+                   ;;(d "\n   [w" n "] starting  work unit: " v " (now: " (now) ")\n")
+                   (let ((timings (time (processor v))))
+                     #t;;(d "\n   [w" n "] completed work unit: " v " in " (cdr timings) "\n")
+                     )
+                   )       
+                 work-queue)
+                (if continue-forever (r)))
+              (d "\nStopping worker [w" n "] - nothing else to do.\n"))))))))
