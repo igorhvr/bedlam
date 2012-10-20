535da609054941eb1fc50102656c305526c2d361
Igor Hjelmstrom Vinhas Ribeiro <igorhvr@iasylum.net>
2012-10-20 19:05:41 -0300

    Performance improvements avoiding using beanshell to call java. Also added blocking tasks processing capability, and reorganized the queue object changing also the names of the messages it accepts.

--- iasylum/work-queue-code.scm
+++ iasylum/work-queue-code.scm
@@ -1,40 +1,57 @@
 ;;; Code by Igor Hjelmstrom Vinhas Ribeiro - this is licensed under GNU GPL v2.
 
+(define-java-classes <java.util.concurrent.linked-blocking-queue> <java.util.concurrent.array-blocking-queue>)
+
+(define-generic-java-methods put take)
+
+ ;; Specification:
+ ;; (if capacity (j "new java.util.concurrent.ArrayBlockingQueue(capacity);" `((capacity ,(->jint capacity)))) (j "new java.util.concurrent.LinkedBlockingQueue();"))
+(define-syntax get-native-blocking-queue
+  (syntax-rules ()
+    ((_ capacity)
+       (if capacity
+           (java-new <java.util.concurrent.array-blocking-queue> (->jint capacity))
+           (java-new <java.util.concurrent.linked-blocking-queue>)))))
 
 (define make-queue
   (lambda* ((capacity #f))
     (let ((inner-queue
-           (if capacity
-               (j "new java.util.concurrent.LinkedBlockingQueue(capacity);" `((capacity ,(->jint capacity))))
-               ;;(j "new java.util.concurrent.ConcurrentLinkedQueue();"))
-               (j "new java.util.concurrent.LinkedBlockingQueue();"))
-           )
+           (get-native-blocking-queue capacity))
           (paused #f))
-      (letrec ((new-function (match-lambda*
-                              [('add v)
-                               (if capacity
-                                   (j "q.put(v);"  `((q ,inner-queue) (v ,(java-wrap v))))
-                                   (j "q.add(v);"  `((q ,inner-queue) (v ,(java-wrap v)))))
+      (letrec ((new-function (match-lambda*                              
+                              [('put-scm v)
+                               ;; Spec (j "q.put(v);"  `((q ,inner-queue) (v ,(java-wrap v))))
+                               (put inner-queue (java-wrap (v)))
+                               ]
+                              [('take-force) 
+                               (java-unwrap (take inner-queue))
                                ]
                               [('put v)
-                               (if capacity
-                                   (j "q.put(v);"  `((q ,inner-queue) (v ,(java-wrap v))))
-                                   (j "q.add(v);"  `((q ,inner-queue) (v ,(java-wrap v)))))
+                               ;; Spec (j "q.put(v);"  `((q ,inner-queue) (v ,(java-wrap v))))
+                               (put inner-queue (if (java-object? v) v (java-wrap v)))
                                ]
-                              [('clear)
-                               (j "q.clear();"  `((q ,inner-queue)))]
-                              [('pause)
-                               (set! paused #t) (void)]
-                              [('resume)
-                               (set! paused #f) (void)]
+                              [('take) 
+                               (if (not paused)
+                                   (let ((result (j "q.take();"  `((q ,inner-queue)))))
+                                     (java-unwrap result))
+                                   'paused)
+                               ]
+                              [('put-java v)
+                               ;; Spec (j "q.put(v);"  `((q ,inner-queue) (v ,(java-wrap v))))
+                               (put inner-queue v)
+                               ]
+                              [('take-java) 
+                               (if (not paused)
+                                   (let ((result (j "q.take();"  `((q ,inner-queue)))))
+                                     result)
+                                   'paused)
+                               ]
                               [('take return-when-paused)
                                (if (not paused)
                                    (let ((result (j "q.take();"  `((q ,inner-queue)))))
                                      (java-unwrap result))
                                    return-when-paused)
                                ]
-                              [('take)
-                               (new-function 'take 'paused)]
                               [('poll return-when-empty)
                                (if (not paused)
                                    (let ((result (j "q.poll();"  `((q ,inner-queue)))))
@@ -42,12 +59,31 @@
                                    return-when-empty)
                                ]
                               [('poll)
-                               (new-function 'poll 'empty)])))
+                               (new-function 'poll 'empty)]
+                              [('clear)
+                               (j "q.clear();"  `((q ,inner-queue)))]
+                              [('pause)
+                               (set! paused #t) (void)]
+                              [('resume)
+                               (set! paused #f) (void)]
+                              [('inner-queue)
+                               inner-queue]
+                              )))
         new-function))))
 
-(define (process-all-work proc queue)
-  (let ((v (queue 'poll)))
-    (if (not (eqv? v 'empty)) (begin (proc v) (process-all-work proc queue)))))
+(define (process-all-work proc queue continue-forever)
+  (if (continue-forever)
+      (let r ()
+        (proc (queue 'take 'paused))
+        (r))
+      (let ((v (queue 'poll)))
+        (if (not (eqv? v 'empty)) (begin (proc v) (process-all-work proc queue))))))
+
+(define (process-all-work-forced proc queue ignored)
+  (let ((q (queue 'inner-queue)))
+    (let r ()
+      (proc (java-unwrap (take q)))
+      (r))))
 
 (define get-next-worker-n
   (let ((m (mutex/new))
@@ -62,7 +98,7 @@
 (define (now) (->string (j "new java.util.Date().toString();")))
 
 (define start-worker
-  (lambda* (processor work-queue (continue-forever #f))
+  (lambda* (processor work-queue (continue-forever #f) (forced #f))
       (thread/spawn
        (lambda ()
          (with-failure-continuation
@@ -71,15 +107,14 @@
           (lambda ()
             (let ((n (get-next-worker-n)))
               (d "\nStarting worker [w" n "]...\n")
-              (let r ()
-                (process-all-work
-                 (lambda (v)
-                   (define (now) (->string (j "new java.util.Date().toString();")))
-                   ;;(d "\n   [w" n "] starting  work unit: " v " (now: " (now) ")\n")
-                   (let ((timings (time (processor v))))
-                     #t;;(d "\n   [w" n "] completed work unit: " v " in " (cdr timings) "\n")
-                     )
-                   )       
-                 work-queue)
-                (if continue-forever (r)))
+              ((if forced process-all-work-forced process-all-work)
+               (lambda (v)
+                 (define (now) (->string (j "new java.util.Date().toString();")))
+                 ;;(d "\n   [w" n "] starting  work unit: " v " (now: " (now) ")\n")
+                 (let ((timings (time (processor v))))
+                   ;;(d "\n   [w" n "] completed work unit: " v " in " (cdr timings) "\n")
+                   #t
+                   ))       
+               work-queue
+               continue-forever)
               (d "\nStopping worker [w" n "] - nothing else to do.\n"))))))))
