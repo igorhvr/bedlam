6ab81a071dd9248276868921cc7241d99bd5037a
Igor Hjelmstrom Vinhas Ribeiro <igorhvr@iasylum.net>
2012-10-29 22:07:47 -0200

    send-email-k now supports key-value lists of To & Cc recipients (in addition to the existing ones).

--- iasylum/email.scm
+++ iasylum/email.scm
@@ -42,17 +42,46 @@
     (lambda* ((smtp-server: mailserver "localhost")
          (recipient-email: recipientemail) (recipient-name: recipientname recipientemail)
          (cc-email: ccemail recipientemail) (cc-name: ccname ccemail)
+         (to-kv: tokv #f)
+         (cc-kv: cckv #f)
          (sender-email: senderemail recipientemail) (sender-name: sendername senderemail)
          (authentication-login: authentication-login "") (authentication-password: authentication-password #f) (use-ssl: use-ssl #f)
          (subject: subject "") (message-text: messagetext "="))
       (log-debug 'send-email mailserver recipientemail recipientname senderemail sendername subject messagetext)
 
+      
+      
       (j 
        "import org.apache.commons.mail.SimpleEmail;
-       SimpleEmail email = new SimpleEmail();
+       email = new SimpleEmail();
        email.setHostName(mailserver);
        email.addTo(recipientemail, recipientname);
-       email.addCc(ccemail, ccname);
+       email.addCc(ccemail, ccname);"
+       `((mailserver ,(->jstring mailserver))
+         (recipientemail ,(->jstring recipientemail))
+         (recipientname ,(->jstring recipientname))
+         (ccemail ,(->jstring ccemail))
+         (ccname ,(->jstring ccname))
+         (senderemail ,(->jstring senderemail))
+         (sendername ,(->jstring sendername))
+         (subject ,(->jstring subject))
+         (messagetext ,(->jstring messagetext))
+         (usessl ,(->jboolean use-ssl))
+         (authenticationlogin ,(->jstring authentication-login))
+         (authenticationpassword ,(->jstring authentication-password))
+         ))
+
+      (when tokv
+        (for-each (match-lambda ((vemail vname)
+                            (j "email.addTo(lccemail, lccname);" `((lccemail ,(->jstring vemail)) (lccname ,(->jstring vname))))))
+                  tokv))
+      
+      (when cckv
+        (for-each (match-lambda ((vemail vname)
+                            (j "email.addCc(lccemail, lccname);" `((lccemail ,(->jstring vemail)) (lccname ,(->jstring vname))))))
+                  cckv))
+      
+      (j "
        if(!\"\".equals(authenticationlogin)) {
            email.setAuthentication(authenticationlogin, authenticationpassword);
        }
