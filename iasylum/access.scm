6000270961db804aeb542faa8f01ee93591c3188
Igor Hjelmstrom Vinhas Ribeiro <igorhvr@iasylum.net>
2012-12-31 14:10:12 -0200

    Added open-access-db-java-io-file function to open a DB from a java.io.File instance instead of the filename.

--- iasylum/access.scm
+++ iasylum/access.scm
@@ -3,10 +3,13 @@
 (require-extension (lib iasylum/jcode))
 
 (module iasylum/access
-  (open-access-db get-table-names get-table get-table-columns)
+  (open-access-db get-table-names get-table get-table-columns open-access-db-java-io-file)
 
   (define (open-access-db fname)
     (j "com.healthmarketscience.jackcess.Database.open(new File(fname));" `((fname ,(->jstring fname)))))
+
+  (define (open-access-db-java-io-file f)
+    (j "com.healthmarketscience.jackcess.Database.open(java_io_file);" `((java_io_file ,f))))
   
   (define (get-table-names db) (iterable->list (j "db.getTableNames();" `((db ,db))) (lambda (v) (->string v))))
   
