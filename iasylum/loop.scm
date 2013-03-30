777ca71dc4389c8364091d041362d658106c5b49
Igor Hjelmstrom Vinhas Ribeiro <igorhvr@iasylum.net>
2013-03-30 00:01:12 -0300

    Added missing exports that prevented the loop macro from being used properly.

--- iasylum/loop.scm
+++ iasylum/loop.scm
@@ -1,6 +1,19 @@
 (require-extension (lib iasylum/match))
 
 (module iasylum/loop
-  (loop)
-  
+  (random-integer let-keyword-form loop %loop %loop-next in-list in-string in-vector in-vector-reverse %in-vector in-port in-file in-range collecting random-real in-random next-permutation! make-vector-range in-permutations next-pair-bucket)
+
+  (define random-integer random)
+
   (include "loop/loop.scm"))
+
+;; Test/sample:
+;;
+;; (define (flatten-x ls)
+;;     (reverse
+;;      (loop lp ((x ls <- in-list ls) (res '()))
+;;          => res
+;;        (if (pair? x)
+;;            (lp res <- (lp ls <- x))
+;;            (lp res <- (cons x res))))))
+;; (flatten-x '(1 (1 2 48 1 3) (1 (2 3 717174 81 (6 1))))) ===> (1 1 2 48 1 3 1 2 3 717174 81 6 1)
