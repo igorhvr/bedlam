5ce047fbbca5fffb26a256edf2f937b3b839a062
Igor Hjelmstrom Vinhas Ribeiro <igorhvr@iasylum.net>
2014-01-31 03:29:09 +0000

    Draft of mail-reading functionality.

--- iasylum/email.scm
+++ iasylum/email.scm
@@ -106,5 +106,64 @@
          (authenticationlogin ,(->jstring authentication-login))
          (authenticationpassword ,(->jstring authentication-password))
          ))))
-    ;; Test (send-email-k 'recipientemail: "igorhvr@iasylum.net") 
+    ;; Test (send-email-k 'recipientemail: "igorhvr@iasylum.net")
+
+  ; WIP.
+  ; Returns a list containing, for each element for which both fn and raw-fn did not return #f, 3-element-lists where the head is the message id and the next two elements are the results of fn and raw-fn
+;  (define receive-imaps-all-inbox-email
+;    (lambda* ((imaps-server-hostname: hostname)
+;              (username: username)
+;              (password: password)
+;              
+;              ;; fn will receive an alist with message data for each message.
+;              ;; The keys are 'subject 'body 'to 'from
+;              ;;              'cc 'message-id 'sent-date 'received-date
+;              ;;              'charset 'attachment-base64
+;              ;;              'spf-status 'dkim-status
+;              (fn: fn identity)
+;              
+;              ;; raw-fn if provided will receive instances of javax.mail.Message
+;              (raw-fn: raw-fn #f))
+;              
+;              (j (quote-convert "//J'
+;                 import java.util.*; import javax.mail.*;
+;                 props = new Properties();
+;                 session = Session.getInstance(props, null);
+;                 Store store = session.getStore('imaps');
+;                 store.connect(hostname, username, password);
+;                 Folder inbox = store.getFolder('INBOX');
+;                 inbox.open(Folder.READ_WRITE);
+;                 
+;                 for (int i = 1; i <= inbox.getMessageCount(); i++) {
+;                     Message msg = inbox.getMessage(i);
+;
+;                     Address[] fromAddresses = msg.getFrom();
+;
+;                     String[] fromAddressesString = new String[address.length];                     
+;                     for (int i = 0 ; i < in.length; i++) {
+;                 	fromAddressesString[i] = fromAddresses[i].toString();
+;                     }
+;                 
+;                     subject = msg.getSubject();
+;                     sentDate = msg.getSentDate();
+;                     content = '';
+;                     
+;                     if (msg.getContent() instanceof Multipart) {
+;                        mp = (Multipart) msg.getContent();
+;                        bp = mp.getBodyPart(0);
+;                        content = bp.getContent();
+;                     } else {
+;                        content = msg.getContent();
+;                     }
+;
+;                     System.out.println('Subject: ' + subject);
+;                     System.out.println('Content: ' + content);
+;                 
+;                     msg.setFlag(Flags.Flag.DELETED, true);
+;                 } 
+;                 inbox.close(true);") `((hostname ,(->jstring hostname))
+;                                        (username ,(->jstring username))
+;                                        (password ,(->jstring password))
+;                                        (fn ,fn)
+;                                        (raw-fn)))))
 )
