8d3719d434164cf18c3f6489c9905bcd5a79be26
Igor Hjelmstrom Vinhas Ribeiro <igorhvr@iasylum.net>
2014-01-31 03:21:37 +0000

    Added logarithm function that optionally receives an alternate base.

--- iasylum/math.scm
+++ iasylum/math.scm
@@ -1,4 +1,6 @@
 (module iasylum/math
-  (fact)
+  (fact log*)
   
-  (define (fact n) (if (or (= 0 n) (= 1 n)) 1 (* n (fact (- n 1))))))
+  (define (fact n) (if (or (= 0 n) (= 1 n)) 1 (* n (fact (- n 1)))))
+  (define* (log* arg1 (arg2 #f)) (if (not arg2) (log arg1) (let ((b arg1) (M arg2)) (/ (log M) (log b)))))
+)  
