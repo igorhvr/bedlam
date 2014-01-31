5ce1b2a150c2f8d21a760e613d7e7c8acebd8d70
Igor Hjelmstrom Vinhas Ribeiro <igorhvr@iasylum.net>
2014-01-31 03:20:16 +0000

    Added utility functions to make it easiear to convert back-and-forth to/from JSON and Scheme.

--- iasylum/json.scm
+++ iasylum/json.scm
@@ -2,6 +2,21 @@
 (require-extension (srfi 69))
 (require-extension (srfi 23))
 (module iasylum/json
-  (json-read json-write)
+  (json-read json-write scheme->json json->scheme)
   
-  (include "json/json-code.scm"))
+  (include "json/json-code.scm")
+  
+  (define (scheme->json structure)
+    (call-with-output-string
+     (lambda (output-port)
+       (json-write
+        structure
+        output-port))))
+  
+  (define (json->scheme string)
+    (call-with-input-string
+     string
+     (lambda (input-port)
+       (json-read input-port))))
+
+)
