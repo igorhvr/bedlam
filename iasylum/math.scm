67f10e71a9f64a95acbe92f575e4c0f8925f8091
Igor Hjelmstrom Vinhas Ribeiro <igorhvr@iasylum.net>
2014-02-13 12:20:28 -0200

    I never remember the name modulo. mod is better.

--- iasylum/math.scm
+++ iasylum/math.scm
@@ -1,6 +1,7 @@
 (module iasylum/math
-  (fact log*)
-  
+  (fact log* mod)
+
+  (define mod modulo)
   (define (fact n) (if (or (= 0 n) (= 1 n)) 1 (* n (fact (- n 1)))))
   (define* (log* arg1 (arg2 #f)) (if (not arg2) (log arg1) (let ((b arg1) (M arg2)) (/ (log M) (log b)))))
 )  
