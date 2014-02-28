4515e422508cfda26bcc96edb59751297480c7a3
Igor Hjelmstrom Vinhas Ribeiro <igorhvr@iasylum.net>
2014-02-28 08:51:50 +0400

    Improved rhino support. Removed run-js/o (it was not working properly) and added non-optimized version of run/s. It allows code that triggers size-related issues to run properly, avoiding errors such as:
    Target exception: org.apache.bsf.BSFException: JavaScript Error: Encountered code generation error while compiling function "39": Program too complex: too big jump offset ((java)#3278)
    >
    bsh.TargetError: Sourced file: inline evaluation of: ``Object jso = manager.eval("javascript", "(java)", 1, 1, code);       jso.toStrin . . . '' : Typed variable declaration : Method Invocation manager.eval : at Line: 1 : in file: inline evaluation of: ``Object jso = manager.eval("javascript", "(java)", 1, 1, code);       jso.toStrin . . . '' : manager .eval ( "javascript" , "(java)" , 1 , 1 , code )
    
    Target exception: org.apache.bsf.BSFException: JavaScript Error: Encountered code generation error while compiling function "39": Program too complex: too big jump offset ((java)#3278)
    
    Another error example:
    
    igorhvr:~/ $ rhino                                                                                      [7:33:16]
    Rhino 1.7 release 4 2013 08 27
    js> load("s.js");
    js: "s.js", line 3278: Encountered code generation error while compiling function "39": Program too complex: too big jump offset
    js>
    
    With optimizations disabled (which is what -opt -1 means) we no longer have issues:
    
    igorhvr:~/ $ rhino -opt -1                                                                              [7:36:33]
    Rhino 1.7 release 4 2013 08 27
    js>  load("s.js");
    js>

--- iasylum/javascript-code.scm
+++ iasylum/javascript-code.scm
@@ -1,15 +1,28 @@
 (define (js-manager)
   (j "org.apache.bsf.BSFManager manager = new org.apache.bsf.BSFManager();   manager;"))
 
+(define (js-manager-no-optimization  (j "new javax.script.ScriptEngineManager().getEngineByName(\"javascript\");")))
+
 (define (run-js/s manager code)
   (->string 
    (j "Object jso = manager.eval(\"javascript\", \"(java)\", 1, 1, code);
       jso.toString();" `((manager ,manager) (code ,(->jstring code))))))
 
-(define (run-js/o manager code)
+(define (run-js-no-optimization/s manager code)
   (->string 
-   (j "Object jso = manager.eval(\"javascript\", \"(java)\", 1, 1, code);
-      jso;" `((manager ,manager) (code ,(->jstring code))))))
+   (j                       
+       "import javax.script.*;
+        cx = org.mozilla.javascript.Context.enter();
+        cx.setOptimizationLevel(-1);
+        ourresult=\"\";
+        try {
+           jso=new ScriptEngineManager().getEngineByName(\"javascript\").eval(code);
+           if(jso!=null) ourresult=jso.toString();
+        } finally {
+           cx.exit();
+        }
+        ourresult;"
+       `((manager ,manager) (code ,(->jstring code))))))
 
 ;; Example: (run-js/s (js-manager) "var f = function (what) { return 'hello, ' + what; }; f('Javascript');")
 
--- iasylum/javascript.scm
+++ iasylum/javascript.scm
@@ -1,6 +1,6 @@
 (require-extension (lib iasylum/jcode))
 
 (module iasylum/javascript
-  (js-manager run-js/s run-js/o)
+  (js-manager js-manager-no-optimization run-js/s run-js-no-optimization/s)
 
   (include "javascript-code.scm"))
