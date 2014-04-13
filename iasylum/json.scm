3a7bfc0f68ef9033b722a53b68f6c706a64e0ea9
Igor Hjelmstrom Vinhas Ribeiro <igorhvr@iasylum.net>
2014-04-13 04:43:44 +0400

    Added an option of forcing all numbers into exact numbers when parsing JSON. This is very useful when huge inexact numbers (greater than the standard SISC precision) are being passed around.

--- iasylum/json.scm
+++ iasylum/json.scm
@@ -12,11 +12,30 @@
        (json-write
         structure
         output-port))))
+
+  (define (decimal-to-fractions-inside-string s)  
+    (irregex-replace/all '(seq
+                           (submatch (? "-"))
+                           (submatch (+ digit))
+                           "."
+                           (submatch (+ digit)))
+                         s
+                         (lambda (m)
+                           (let ((sign (irregex-match-substring m 1))
+                                 (beforedot (irregex-match-substring m 2))
+                                 (afterdot (irregex-match-substring m 3)))
+                             (number->string
+                              (* (if (string=? "-" sign) -1 1)
+                                 (+ (string->number beforedot)
+                                    (/ (string->number afterdot) (expt 10 (string-length afterdot))))))))))
   
-  (define (json->scheme string)
-    (call-with-input-string
-     string
-     (lambda (input-port)
-       (json-read input-port))))
+  (define json->scheme
+    (lambda* (string (force-exact #f))
+             (call-with-input-string
+                 (if force-exact
+                     (decimal-to-fractions-inside-string string)
+                     string)
+               (lambda (input-port)
+                 (json-read input-port)))))
 
 )
