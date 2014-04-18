742aad6088a3cb884184af5d7f8836e2ca1c2072
Igor Hjelmstrom Vinhas Ribeiro <igorhvr@iasylum.net>
2014-04-19 02:39:22 +0400

    Added exact-floor implementation to math repertoire. This works for huge exact numbers (fractions).

--- iasylum/math.scm
+++ iasylum/math.scm
@@ -1,6 +1,10 @@
 (module iasylum/math
-  (fact log* mod)
+  (fact log* mod exact-floor)
 
+  (define (exact-floor n)
+    (let ((n (inexact->exact n)))
+      (let ((up (numerator n)) (down (denominator n))) (/ (- up (mod up down)) down))))
+  
   (define mod modulo)
   (define (fact n) (if (or (= 0 n) (= 1 n)) 1 (* n (fact (- n 1)))))
   (define* (log* arg1 (arg2 #f)) (if (not arg2) (log arg1) (let ((b arg1) (M arg2)) (/ (log M) (log b)))))
