75b1dd711c26b73d6db6e1f0b4e1cea47a34c6b4
Igor Hjelmstrom Vinhas Ribeiro <igorhvr@iasylum.net>
2014-08-10 04:27:07 +0400

    Removing separate .java file for extract-filepaths (which is redundant with rglob anyway).

--- iasylum/init-code.scm
+++ iasylum/init-code.scm
@@ -430,7 +430,31 @@
          (let ((full-path (->string full-path-file)))
            (and (not (member full-path except))
                 (add-lib-fullpath full-path))))
-       (->list (j (file->string "/base/bedlam/iasylum/extract-filepaths.java")
+       (->list (j "
+                   public static java.util.Collection<String> extractAllPaths(java.util.Collection<String> currentList, java.io.File fileOrDir) throws java.io.IOException {
+		    if (fileOrDir.exists()) {
+			if (fileOrDir.isFile()) {
+			    currentList.add(fileOrDir.getAbsolutePath());
+			} else if (fileOrDir.isDirectory()) {
+			    java.io.File[] files = fileOrDir.listFiles();
+			    
+			    if (files != null) {
+				for (java.io.File file : files) {
+				    extractAllPaths(currentList, file);
+				}
+			    }
+			}
+		    }
+		    
+		    return currentList;
+		}
+		
+		public static java.util.Collection<String> extractAllPaths(String path) throws java.io.IOException {
+		    return extractAllPaths(new java.util.ArrayList<String>(100), new java.io.File(path));
+		}
+		
+		return extractAllPaths(input).toArray();
+		"
                   `((input ,(->jstring path)))))))
 
 (add-lib-recursively "/base/bedlam/jars/web")
