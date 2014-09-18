2e42b2161ee2d3b42e5206db28860624dbd9f358
Igor Hjelmstrom Vinhas Ribeiro <igorhvr@iasylum.net>
2014-09-18 01:31:28 -0300

    Changed the default match implementation to matchable, which gives us better ellipsis support in patterns.

--- iasylum/match.scm
+++ iasylum/match.scm
@@ -1,11 +1,8 @@
 ;;; See match directory for match license.
 (module iasylum/match
-  
-  (match-syntax-error match match-next match-one match-two match-quasiquote match-quasiquote-step match-drop-ids match-gen-or match-gen-or-step match-gen-ellipses match-verify-no-ellipses match-vector match-vector-two match-vector-step match-vector-ellipses match-vector-tail match-vector-tail-two match-extract-vars match-extract-vars-step match-extract-quasiquote-vars match-extract-quasiquote-vars-step match-lambda match-lambda* match-let match-letrec match-let/helper match-named-let match-let* match-check-ellipse match-check-identifier syntax-symbol-append-?)
+ 
+ (match match-check-ellipse match-check-identifier match-drop-ids match-extract-quasiquote-vars match-extract-quasiquote-vars-step match-extract-vars match-extract-vars-step match-gen-ellipses match-gen-or match-gen-or-step match-lambda match-lambda* match-let match-let* match-let/helper match-letrec match-named-let match-next match-one match-quasiquote match-quasiquote-step match-record-refs match-syntax-error match-two match-vector match-vector-ellipses match-vector-step match-vector-tail match-vector-tail-two match-vector-two match-verify-no-ellipses syntax-symbol-append-?)
 
-  (include "match/match-cond-expand.scm")
+ (include "match/other/matchable/matchable_adapted.scm")
 
-  )
-
-
-
+ )
