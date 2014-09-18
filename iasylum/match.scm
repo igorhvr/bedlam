f5898283fd1a60488ca31b102dc4d540dec4c55f
Igor Hjelmstrom Vinhas Ribeiro <igorhvr@iasylum.net>
2014-09-18 16:51:21 +0000

    Removed chicken-specific match export (causing issues during loading of matchable match library).

--- iasylum/match.scm
+++ iasylum/match.scm
@@ -1,7 +1,7 @@
 ;;; See match directory for match license.
 (module iasylum/match
  
- (match match-check-ellipse match-check-identifier match-drop-ids match-extract-quasiquote-vars match-extract-quasiquote-vars-step match-extract-vars match-extract-vars-step match-gen-ellipses match-gen-or match-gen-or-step match-lambda match-lambda* match-let match-let* match-let/helper match-letrec match-named-let match-next match-one match-quasiquote match-quasiquote-step match-record-refs match-syntax-error match-two match-vector match-vector-ellipses match-vector-step match-vector-tail match-vector-tail-two match-vector-two match-verify-no-ellipses syntax-symbol-append-?)
+ (match match-check-ellipse match-check-identifier match-drop-ids match-extract-quasiquote-vars match-extract-quasiquote-vars-step match-extract-vars match-extract-vars-step match-gen-ellipses match-gen-or match-gen-or-step match-lambda match-lambda* match-let match-let* match-let/helper match-letrec match-named-let match-next match-one match-quasiquote match-quasiquote-step match-syntax-error match-two match-vector match-vector-ellipses match-vector-step match-vector-tail match-vector-tail-two match-vector-two match-verify-no-ellipses syntax-symbol-append-?)
 
  (include "match/other/matchable/matchable_adapted.scm")
 
