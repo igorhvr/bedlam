286ac801f8edba29caa565872e4cca8a5551ee50
Igor Hjelmstrom Vinhas Ribeiro <igorhvr@iasylum.net>
2015-01-25 00:42:39 +0000

    Added method to be able to POST the contents of a string to a given URL.

--- iasylum/net.scm
+++ iasylum/net.scm
@@ -21,6 +21,27 @@
 
         result;"
         `((destinationurl ,(->jstring destinationUrl))))))
+
+  (define (http-call-post-string/string destinationUrl contents)
+    (->string
+     (j "httpclient = org.apache.http.impl.client.HttpClients.createDefault();
+        httppost = new org.apache.http.client.methods.HttpPost(destinationurl);
+        httppost.setEntity(new org.apache.http.entity.StringEntity(contents));
+        response = httpclient.execute(httppost);
+        result=\"\";
+        try {
+          tent=response.getEntity();
+          result=org.apache.http.util.EntityUtils.toString(tent);
+        } catch(Exception e) {
+          throw new RuntimeException(e);
+        } finally {
+          response.close();
+        }
+
+        result;"
+        `((destinationurl ,(->jstring destinationUrl))
+          (contents ,(->jstring contents)))
+        )))
   )
 
   
