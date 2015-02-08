f6fef05df6a4b2a9892ff5b319d0a480fe737c31
Igor Hjelmstrom Vinhas Ribeiro <igorhvr@iasylum.net>
2015-02-08 02:32:56 +0000

    Doc: a comment with a few details about our crypto library.

--- iasylum/crypto.scm
+++ iasylum/crypto.scm
@@ -1,5 +1,6 @@
 (require-extension (lib iasylum/javascript))
 
+;; This is a wrapper of the sjcl designed to allow encrypted exchanges between a bedlam-based service and a js-based peer.
 (module iasylum/crypto
   (generate-keypair
    asymmetric-encrypt asymmetric-decrypt
