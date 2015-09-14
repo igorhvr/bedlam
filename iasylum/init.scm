e29a5c097b3370ae1d6681ffd1babcf8ebe60db7
Igor Hjelmstrom Vinhas Ribeiro <igorhvr@iasylum.net>
2015-09-14 14:59:14 +0000

    Use compiled version of init-code for faster loading.

--- iasylum/init.scm
+++ iasylum/init.scm
@@ -4,4 +4,4 @@
 ; (begin (define iasylum-bedlam-location "/home/igorhvr/idm/bedlam/") (load (string-append iasylum-bedlam-location "iasylum/init.scm")))
 
 (if (not (getprop 'bedlam-loaded-and-ready-to-use (interaction-environment)))
-    (load "init-code.scm"))
+    (load "init-code.scc"))
