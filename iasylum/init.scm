53ca39a417047b83311a0f2deef9f60f19551957
Igor Hjelmstrom Vinhas Ribeiro <igorhvr@iasylum.net>
2015-09-14 16:15:03 +0000

    Made standard r5rs stuff available to compiled version of init-code

--- iasylum/init.scm
+++ iasylum/init.scm
@@ -3,5 +3,9 @@
 ; Usage example:
 ; (begin (define iasylum-bedlam-location "/home/igorhvr/idm/bedlam/") (load (string-append iasylum-bedlam-location "iasylum/init.scm")))
 
+(define @r5rs::string-append string-append)
+(define @r5rs::cons cons)
+(define @r5rs::list list)
+
 (if (not (getprop 'bedlam-loaded-and-ready-to-use (interaction-environment)))
     (load "init-code.scc"))
