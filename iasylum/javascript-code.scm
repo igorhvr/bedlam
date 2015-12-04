e5c5f5722c527a9ddaa6f495baf5473994d62437
Igor Hjelmstrom Vinhas Ribeiro <igorhvr@iasylum.net>
2015-12-04 22:48:13 +0000

    Testing javascript implementation using j2v8.

--- iasylum/javascript-code.scm
+++ iasylum/javascript-code.scm
@@ -1,51 +1,51 @@
-;;nodejs - remote
-(define (nodejs-global code)
-  (log-trace "About to execute js code" code)
-  (let ((result (http-call-post-string/string "http://js:27429/" code)))
-    (log-trace "Result" result)
-    (if (and result
-             (not (string=? "" result)))
-        (json->scheme result)
-        #f)))
-
-(define* (nodejs code (vars '()))
-  (let* ((names-and-values-pair (call-with-values
-                                    (lambda () (unzip2 vars))
-                                  (lambda (names values) (cons names values))))
-         (names (car names-and-values-pair))
-         (values (cdr names-and-values-pair))
-         (code (format "(function(~a) { return ~a })(~a);"
-                       (apply add-between (cons "," names))
-                       code
-                       (apply add-between (cons "," (map (lambda (v)
-                                                           (cond [(number? v)
-                                                                  (number->string v)]
-                                                                 [else
-                                                                  (string-append* "\"" v "\"")]))
-                                                         values))))))
-    (node-js-global code)))
-
-;; rhino
-(define (js-manager)  (j "new javax.script.ScriptEngineManager().getEngineByName(\"rhino\");"))
-
-(define-generic-java-method get |get|)
-(define-generic-java-method set |set|)
-
-(define (create-thread-local-javascript-manager-retriever)
-    (let ((tl
-           (j "mtl=new ThreadLocal() {
-                   protected synchronized Object initialValue() {                       
-                       return new javax.script.ScriptEngineManager().getEngineByName(\"rhino\");
-                   }
-               }; mtl;")))
-      (lambda ()
-        (let ((result (get tl)))
-          result))))
-
-(define get-local-javascript-manager)
-(define js-rhino)
-
-;; v8
+;; ;;nodejs - remote
+;; (define (nodejs-global code)
+;;   (log-trace "About to execute js code" code)
+;;   (let ((result (http-call-post-string/string "http://js:27429/" code)))
+;;     (log-trace "Result" result)
+;;     (if (and result
+;;              (not (string=? "" result)))
+;;         (json->scheme result)
+;;         #f)))
+;; 
+;; (define* (nodejs code (vars '()))
+;;   (let* ((names-and-values-pair (call-with-values
+;;                                     (lambda () (unzip2 vars))
+;;                                   (lambda (names values) (cons names values))))
+;;          (names (car names-and-values-pair))
+;;          (values (cdr names-and-values-pair))
+;;          (code (format "(function(~a) { return ~a })(~a);"
+;;                        (apply add-between (cons "," names))
+;;                        code
+;;                        (apply add-between (cons "," (map (lambda (v)
+;;                                                            (cond [(number? v)
+;;                                                                   (number->string v)]
+;;                                                                  [else
+;;                                                                   (string-append* "\"" v "\"")]))
+;;                                                          values))))))
+;;     (node-js-global code)))
+;; 
+;; ;; rhino
+;; (define (js-manager)  (j "new javax.script.ScriptEngineManager().getEngineByName(\"rhino\");"))
+;; 
+;; (define-generic-java-method get |get|)
+;; (define-generic-java-method set |set|)
+;; 
+;; (define (create-thread-local-javascript-manager-retriever)
+;;     (let ((tl
+;;            (j "mtl=new ThreadLocal() {
+;;                    protected synchronized Object initialValue() {                       
+;;                        return new javax.script.ScriptEngineManager().getEngineByName(\"rhino\");
+;;                    }
+;;                }; mtl;")))
+;;       (lambda ()
+;;         (let ((result (get tl)))
+;;           result))))
+;; 
+;; (define get-local-javascript-manager)
+;; (define js-rhino)
+;; 
+;; ;; v8
 (define js)
 (define get-local-javascript-v8-runtime)
 (define js-v8)
@@ -63,51 +63,51 @@
         (let ((result (get tl)))
           result))))
 
-;; rhino
-(set! get-local-javascript-manager (create-thread-local-javascript-manager-retriever))
-
-(set! js-rhino
-  (lambda*
-   (code (vars #f) (manager (get-local-javascript-manager)))
-   ;(d/n "Will run code: (((((" code "))))) saved to " (save-to-somewhere code))
-   ;(d/n "vars: " vars)
-   (j                       
-       "import javax.script.*;
-        cx = org.mozilla.javascript.Context.enter();
-        cx.setOptimizationLevel(-1);"        
-       `((manager ,manager)))
-  
-  (when vars
-    (for-each
-     (lambda (v)
-       (match-let ( ( (vname vvalue) v ) )
-                  (begin
-                    (let* ((sname (if (string? vname) vname (symbol->string vname)))
-                           (name (->jstring sname )))
-                      ;(d/n "saving " sname " to " (save-to-somewhere vvalue))
-                      (j "manager.put(jsobjectname, jsobjectvalue);"
-                         `((manager ,manager)
-                           (jsobjectname ,(->jstring sname))
-                           (jsobjectvalue ,(->jobject vvalue))))))))
-     vars))
-
-  (let ((final-result
-   (j                       
-       "ourresult=null;
-        try {           
-           jso=manager.eval(code);
-           if(jso!=null) ourresult=jso;
-        } finally {
-           cx.exit();
-        }
-        ourresult;"
-       `((manager ,manager) (code ,(->jstring code))))))
-    ;(d/n "final-result: " (save-to-somewhere final-result))
-    final-result
-    )))
-
-;; Example: (run-js/s (js-manager) "var f = function (what) { return 'hello, ' + what; }; f('Javascript');")
-
+;; ;; rhino
+;; (set! get-local-javascript-manager (create-thread-local-javascript-manager-retriever))
+;; 
+;; (set! js-rhino
+;;   (lambda*
+;;    (code (vars #f) (manager (get-local-javascript-manager)))
+;;    ;(d/n "Will run code: (((((" code "))))) saved to " (save-to-somewhere code))
+;;    ;(d/n "vars: " vars)
+;;    (j                       
+;;        "import javax.script.*;
+;;         cx = org.mozilla.javascript.Context.enter();
+;;         cx.setOptimizationLevel(-1);"        
+;;        `((manager ,manager)))
+;;   
+;;   (when vars
+;;     (for-each
+;;      (lambda (v)
+;;        (match-let ( ( (vname vvalue) v ) )
+;;                   (begin
+;;                     (let* ((sname (if (string? vname) vname (symbol->string vname)))
+;;                            (name (->jstring sname )))
+;;                       ;(d/n "saving " sname " to " (save-to-somewhere vvalue))
+;;                       (j "manager.put(jsobjectname, jsobjectvalue);"
+;;                          `((manager ,manager)
+;;                            (jsobjectname ,(->jstring sname))
+;;                            (jsobjectvalue ,(->jobject vvalue))))))))
+;;      vars))
+;; 
+;;   (let ((final-result
+;;    (j                       
+;;        "ourresult=null;
+;;         try {           
+;;            jso=manager.eval(code);
+;;            if(jso!=null) ourresult=jso;
+;;         } finally {
+;;            cx.exit();
+;;         }
+;;         ourresult;"
+;;        `((manager ,manager) (code ,(->jstring code))))))
+;;     ;(d/n "final-result: " (save-to-somewhere final-result))
+;;     final-result
+;;     )))
+;; 
+;; ;; Example: (run-js/s (js-manager) "var f = function (what) { return 'hello, ' + what; }; f('Javascript');")
+;; 
 
 (set! get-local-javascript-v8-runtime (create-thread-local-javascript-v8-runtime-retriever))
 
