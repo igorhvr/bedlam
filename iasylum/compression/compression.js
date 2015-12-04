91f7a381c272a26f30eca5678a4b39f0f399aa74
Igor Hjelmstrom Vinhas Ribeiro <igorhvr@iasylum.net>
2015-12-04 00:45:21 +0000

    lzw Java & Javascript quick'n'dirty compression support. (fixed)

--- iasylum/compression/compression.js
+++ iasylum/compression/compression.js
@@ -8,11 +8,11 @@ if (!iasylum || !LZString) {
 }
 
 var iasylum_compression = {
-    'lzw-compress': function () {
+    'lzw-compress': function (data) {
         return LZString.compress(data);
     },
 
-    'lzw-uncompress': function () {
+    'lzw-uncompress': function (data) {
         return LZString.decompress(data);
     }
 
