3ffc192f180c17568d7c6f3da32350802f433def
Igor Hjelmstrom Vinhas Ribeiro <igorhvr@iasylum.net>
2015-12-04 17:31:06 +0000

    Debugging.

--- iasylum/compression/compression-code.scm
+++ iasylum/compression/compression-code.scm
@@ -1,12 +1,14 @@
 (define (lzw-compress value)
   (d/n "About to compress " value)
   (let ((result (->string (j "com.diogoduailibe.lzstring4j.LZString.compressToBase64(v);" `((v ,(->jstring value)))))))
-    (d/n "Success. Result: " value)))
+    (d/n "Success. Result: " result)
+    result))
 
 (define (lzw-decompress value)
   (d/n "About to decompress " value)
   (let ((result (->string (j "com.diogoduailibe.lzstring4j.LZString.decompressFromBase64(v);" `((v ,(->jstring value)))))))
-    (d/n "Success. Result: " value)))
+    (d/n "Success. Result: " result)
+    result))
 
 
 ;; Sample roundtrip working:
