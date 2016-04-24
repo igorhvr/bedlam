6ad926b01f91f7837355da7ab9765701e80698d5
Igor Hjelmstrom Vinhas Ribeiro <igorhvr@iasylum.net>
2016-04-24 00:42:41 +0000

    Additional random-related utility functions: secure-random-byte sort-of-secure-random-byte pseudo-random-uuid random-string random-var random->integer base58-bitcoin-like-random-code base26-random-code-upper.

--- iasylum/random.scm
+++ iasylum/random.scm
@@ -4,7 +4,16 @@
 (require-extension (lib iasylum/srfi-89))
 
 (module iasylum/random
-  (random scheme-random random-maker)
+  (random scheme-random random-maker
+          
+          secure-random-byte
+          sort-of-secure-random-byte
+          pseudo-random-uuid
+          random-string
+          random-var
+          random->integer
+          base58-bitcoin-like-random-code
+          base26-random-code-upper)
   (import s2j)
   (include "random-code.scm")
   )
