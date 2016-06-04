a0f188b995d9172b9dd427c0a2c39eb464f22d16
Igor Hjelmstrom Vinhas Ribeiro <igorhvr@iasylum.net>
2016-06-04 22:14:48 +0000

    Sample tomcat-based app for bedlam.

--- siscweb/bedlam-apache-tomcat-6.0.18/webapps/ROOT/WEB-INF/scm/web/v0/d.scm
+++ siscweb/bedlam-apache-tomcat-6.0.18/webapps/ROOT/WEB-INF/scm/web/v0/d.scm
@@ -2,6 +2,7 @@
 (require-extension (lib siscweb/request))
 (require-extension (lib siscweb/response))
 (require-extension (lib siscweb/html))
+(require-extension (lib siscweb/xhtml))
 
 (extend-classpath "/base/bedlam/siscweb/bedlam-apache-tomcat-6.0.18/webapps/ROOT/WEB-INF/classes/")
 
@@ -21,16 +22,11 @@
 (define i18n.js (file->string "/base/bedlam/siscweb/bedlam-apache-tomcat-6.0.18/webapps/ROOT/s/i18n.js"))
 (define dic.js (file->string "/base/bedlam/siscweb/bedlam-apache-tomcat-6.0.18/webapps/ROOT/s/dic.js"))
 
-(js dic.js)
-(js i18n.js)
-
 (define get-message
   (lambda* (message (lang "pt"))
-      (if (and (irregex-match '(* (or alphanumeric (" áàéèíìóòúùãẽĩõũçćñ¿?¡!%/$()_.,-+=/\\?|#$&*"))) lang)
-               (irregex-match '(* (or alphanumeric (" áàéèíìóòúùãẽĩõũçćñ¿?¡!%/$()_.,-+=/\\?|#$&*"))) message))
-          ;; TODO: pass parameters instead of string contact (broken).
-          (->string (js (string-append "getMessage_engine(\"" message "\", \"" lang "\");")))
-          (string-append lang "_" message "_untranslated"))))
+           (js dic.js)
+           (js i18n.js)
+           (->string (js "getMessage_engine(messagei18n, langi18n);" `((messagei18n ,(->jstring message)) (langi18n ,(->jstring lang)))))))
 
 (define (send-json json)
   (send-html/back
@@ -52,5 +48,72 @@
     (loop)
     (let ((final-result (get-output-string o)))
       final-result))
+
+(publish/wildcard
+  "/hello-world/*"
+  (lambda (request)
+    (set! request #f)
+    (send-html/back
+    `(html
+      (head (title "Hello, world! in two languages"))
+      (body (p ,(get-message "hello" "pt")) (p ,(get-message "hello" "en")))))))
+
+
+ (define (hello-world request)
+    (let loop ()
+      (for-each
+       (lambda (message)
+         (send-xhtml/suspend
+          (make-hello-world-page message)))
+       messages)
+      (loop)))
+
+(publish
+ "/text-accumulator"
+  (lambda (request)
+    (let ((whatever (make-parameter* "")))
+      (let loop ()      
+        (send-html/suspend
+         (lambda (k-url)
+           (let ((datafield (extract-single-binding "datafield" (get-bindings request))))
+             (whatever (string-append* (whatever) datafield))
+             `(html (head (title ,"Text accumulator"))
+                    (body
+                     (h3 "Text Accumulator")
+                     (p ,(whatever))
+                     
+                     (form (@ (action ,k-url))
+                           (input (@ (type "text") (name "datafield")))
+                           (input (@ (type "submit") (name "ACCUMULATE!")))))))))
+        
+        (loop)))))
+
+(publish
+ "/three-steps-example"
+  (lambda (request)
+    (let ((whatever (make-parameter* "")))
+      (send-html/suspend
+         (lambda (k-url)
+           `(html (head (title ,"STEP 1"))
+                    (body
+                     (h3 "First step")
+                     
+                     (form (@ (action ,k-url))
+                           (input (@ (type "text") (name "datafield")))
+                           (input (@ (type "submit") (value "Go to second"))))))))
+
+      (send-html/suspend
+         (lambda (k-url)
+           `(html (head (title ,"STEP 2"))
+                    (body
+                     (h3 "Second step " ,(extract-single-binding "datafield" (get-bindings request)))
+                     
+                     (form (@ (action ,k-url))
+                           (input (@ (type "submit") (value "GO TO FINAL PLACE."))))))))
+
+      (send-html/finish
+       `(html (head (title ,"STEP 3 - FINISHED! Thanks!"))
+              (body
+               (h3 "END.")))))))
 
 (log-info 'd-startup-finished)
