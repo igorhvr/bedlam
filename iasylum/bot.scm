48124dc8b873ed499a2cc27aa8320ee8400e54d6
Felipe Micaroni Lalli <micaroni@gmail.com>
2016-07-01 15:36:13 -0300

    Minor: removing spaces.

--- iasylum/bot.scm
+++ iasylum/bot.scm
@@ -9,7 +9,7 @@
     (lambda* ((in-work-queue: in-work-queue) (out-work-queue: out-work-queue) (name: name) (server-hostname: server-hostname) (server-port: server-port) (server-password: server-password) (channel: channel) )
              (let* ((inner-queue-out-work-queue (out-work-queue 'inner-queue))
                     (pmtoutqueuevarname (random-var))
-                    (bot                     
+                    (bot
                      (j (string-append "new org.pircbotx.PircBotX(new org.pircbotx.Configuration.Builder().setName(pmtname).setSocketFactory(javax.net.ssl.SSLSocketFactory.getDefault()).setCapEnabled(true).addCapHandler(new org.pircbotx.cap.TLSCapHandler(new org.pircbotx.UtilSSLSocketFactory().trustAllCertificates(), true)).addListener(new org.pircbotx.hooks.ListenerAdapter(){public void onMessage(org.pircbotx.hooks.events.MessageEvent event) {if(mchannel.equals(event.getChannel().getName())) " pmtoutqueuevarname ".put(new sisc.data.ImmutableString(event.getMessage()));}}).setServerHostname(pmtserverhostname).setServerPort(pmtserverport).setServerPassword(pmtserverpassword).buildConfiguration());")
                         `((pmtname ,(->jstring name))
                           (,(string->symbol pmtoutqueuevarname) ,inner-queue-out-work-queue)
