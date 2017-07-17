6e59d32b592818829d48cc42489df1b2f26f5e97
Igor Hjelmstrom Vinhas Ribeiro <igorhvr@iasylum.net>
2017-07-16 22:48:58 -0300

    We now expose specific Javascript engines to allow callers with some specific dependency to force one to be used.

--- iasylum/javascript.scm
+++ iasylum/javascript.scm
@@ -2,7 +2,8 @@
 (require-extension (lib iasylum/net))
 
 (module iasylum/javascript
-  (js-manager create-thread-local-javascript-manager-retriever get-local-javascript-manager js
+  (js-manager create-thread-local-javascript-manager-retriever get-local-javascript-manager
+   js js-v8 js-rhino
    nodejs nodejs-global)
 
   (include "javascript-code.scm"))
