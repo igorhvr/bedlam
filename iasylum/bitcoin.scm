df54c42c2a6fa9abced78df20d0ba6c7ae1c5218
Igor Hjelmstrom Vinhas Ribeiro <igorhvr@iasylum.net>
2018-05-01 23:16:38 -0300

    Better name for bitcoin signature check predicate.

--- iasylum/bitcoin.scm
+++ iasylum/bitcoin.scm
@@ -1,7 +1,7 @@
 (module iasylum/bitcoin
-  (is-valid-signature test-is-valid-signature)
+  (bitcoin/valid-signature? test-bitcoin/valid-signature?)
 
-  (define (is-valid-signature bitcoin-address signature message)
+  (define (bitcoin/valid-signature? bitcoin-address signature message)
     (->boolean
      (j "try{
        return org.bitcoinj.core.LegacyAddress.fromKey(
@@ -12,6 +12,6 @@
         return false;
     }" `((message ,(->jstring message)) (signature ,(->jstring signature)) (address ,(->jstring bitcoin-address))))))
 
-  (define (test-is-valid-signature)
-    (assert (is-valid-signature "1MjFv4bNHjDnD8FdyAgvrEEDaVEpfvyi7h" "H0vLPZxD8Yc9hv0YkobbzhQCiNxvjglaHR/8LDqOLr3TOkJveN094R6EBWsnD5BKx5czAjJrgJqUNiiChNq8Sgk=" "Quero trabalhar na vanguarda da economia digital!"))
-    (assert (not (is-valid-signature "1MjFv4bNHjDnD8FdyAgvrEEDaVEpfvyi7h" "H0vLPZxD8Yc9hvYkobbzhQCiNxvjglaHR/8LDqOLr3TOkJveN094R6EBWsnD5BKx5czAjJrgJqUNiiChNq8Sgk=" "Quero trabalhar na vanguarda da economia digital!")))))
+  (define (test-bitcoin/valid-signature?)
+    (assert (bitcoin/valid-signature? "1MjFv4bNHjDnD8FdyAgvrEEDaVEpfvyi7h" "H0vLPZxD8Yc9hv0YkobbzhQCiNxvjglaHR/8LDqOLr3TOkJveN094R6EBWsnD5BKx5czAjJrgJqUNiiChNq8Sgk=" "Quero trabalhar na vanguarda da economia digital!"))
+    (assert (not (bitcoin/valid-signature? "1MjFv4bNHjDnD8FdyAgvrEEDaVEpfvyi7h" "H0vLPZxD8Yc9hvYkobbzhQCiNxvjglaHR/8LDqOLr3TOkJveN094R6EBWsnD5BKx5czAjJrgJqUNiiChNq8Sgk=" "Quero trabalhar na vanguarda da economia digital!")))))
