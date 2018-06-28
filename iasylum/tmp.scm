e429d5a7427eeeaae666bfa3afe9063fd30defea
Igor Hjelmstrom Vinhas Ribeiro <igorhvr@iasylum.net>
2018-06-28 13:55:53 -0300

    Scratch code to retrieve maximum amount of memory available within the JVM from the runtime. Keywords: see Xmx for a JVM.

--- iasylum/tmp.scm
+++ iasylum/tmp.scm
@@ -142,3 +142,6 @@
 ;Generates lines to compile each scheme file....
 
 (each-for (filter (lambda (v) (irregex-search "scm$" v)) (rglob "/home/igorhvr/idm/bedlam/iasylum")) (lambda (v) (d/n (string-append* "/base/bedlam/sisc/sisc-1.16.6/sisc -e '(define iasylum-bedlam-location \"/base/bedlam/\") (load (string-append iasylum-bedlam-location \"iasylum/init.scm\")) (d/n \"Will compile\" \"" v "\")(with/fc (lambda p (display p) (j \"System.exit(0);\")) (lambda () (smart-compile \"" v "\")))(j \"System.exit(0);\")'"))))
+
+(define (jvm-max-memory)
+ ->number (j "Runtime.getRuntime().maxMemory();"))
