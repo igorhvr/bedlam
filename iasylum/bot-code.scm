4aaa0f604b4125315832533840f78cddb240131e
Igor Hjelmstrom Vinhas Ribeiro <igorhvr@iasylum.net>
2019-07-25 02:56:40 +0100

    Work queue bots can now work with both private groups and channels.

--- iasylum/bot-code.scm
+++ iasylum/bot-code.scm
@@ -1,51 +1,58 @@
 (define work-queue-bot
-  (lambda* ((in-work-queue: in-work-queue) (name: name) (channel: channel) (token: token #f))
+  (lambda* ((in-work-queue: in-work-queue)
+            (out-work-queue: out-work-queue #f)
+            (name: name) (channel: channel) (token: token))
+
 	   (watched-thread/spawn (lambda()
 				   (mutex/synchronize (mutex-of clj) (lambda () (clj "(require 'clj-slack.chat)")))
 				   (let loop ()
-				     (let* ((msg (in-work-queue 'take))
-					    (nmsg (string-append "PRIVMSG #" channel " :" msg)))
-				       (if token
-					   (mutex/synchronize (mutex-of clj)
-							      (lambda ()
-								(clj "(require 'clj-slack.chat)
+				     (let* ((msg (in-work-queue 'take)))
+                                       (mutex/synchronize (mutex-of clj)
+                                                          (lambda ()
+                                                            (clj "(require 'clj-slack.chat)
                                                                                   (clj-slack.chat/post-message
                                                                                   {:api-url \"https://slack.com/api\" :token strtoken}
                                                                                   strchannel  strmsg {:username strusername})"
-								     `((strchannel ,(->jstring channel))
-								       (strtoken ,(->jstring token))
-								       (strusername ,(->jstring name))
-								       (strmsg ,(->jstring msg))
-								       )
-								     )
-								))
-					   (begin
-					     (j (format "~a.sendRawLineToServer(ln);" botvar)
-						`((,botvar ,bot)
-						  (ln ,(->jstring nmsg))))
-					     ;; To avoid getting disconnected 
-					     (sleep 2000)))
-
+                                                                 `((strchannel ,(->jstring channel))
+                                                                   (strtoken ,(->jstring token))
+                                                                   (strusername ,(->jstring name))
+                                                                   (strmsg ,(->jstring msg))
+                                                                   )
+                                                                 )
+                                                            ))
 				       (loop)))))
+           (when out-work-queue
+             (create-reader-bot
+              'name: name
+              'token: token
+              'channels:  (list (cons channel (out-work-queue 'inner-queue)))))
+           
 	   #t))
 
 (define create-reader-bot
-    (lambda* ((name: name) (channels: channels) (token: token #f))
+    (lambda* ((name: name) (channels: channels) (token: token))
              (let* ((channelsvar (random-var))
 		    (tokenvar (random-var)))
                (watched-thread/spawn (lambda ()
-				       (mutex/synchronize (mutex-of clj) (lambda () (clj "(require '[slack-rtm.core :as rtm]) (require '[clj-slack.groups :as groups]) (require '[clojure.tools.logging :as log])")))
+				       (mutex/synchronize (mutex-of clj) (lambda () (clj "(require '[slack-rtm.core :as rtm]) (require '[clj-slack.channels :as channels]) (require '[clj-slack.groups :as groups]) (require '[clojure.tools.logging :as log])")))
 				       (clj
 					(format
                                          "(require '[slack-rtm.core :as rtm])
+                                          (require '[clj-slack.channels :as channels])
                                           (require '[clj-slack.groups :as groups])
                                           (require '[clojure.tools.logging :as log])
                                           (def conn (atom nil))
 
-                                          (def channels-id (into {} (map
+                                          (def channels-id (into {}
+                                                             (concat
+                                                                    (map
+                                                                     (fn [channel]
+                                                                         {(get channel :id) (get channel :name)})
+                                                                         (get (channels/list {:api-url \"https://slack.com/api\" :token ~a}) :channels))
+                                                                    (map
                                                                      (fn [group]
                                                                          {(get group :id) (get group :name)})
-                                                                         (get (groups/list {:api-url \"https://slack.com/api\" :token ~a}) :groups))))
+                                                                         (get (groups/list {:api-url \"https://slack.com/api\" :token ~a}) :groups)))))
 
                                           (defn message-receiver [e]
                                                 (when (and (contains? ~a (get channels-id (get e :channel)))
@@ -58,11 +65,13 @@
                                                 (reset! conn (rtm/rtm-connect token
                                                                               :on-close (fn [{:keys [status reason]}]
                                                                                             (log/error reason \"Previous connection with Slack was closed, retrying in 3 seconds...\")
-                                                                                            (Thread/sleep 3000) ;; Waiting 3s before trying again
+                                                                                            (Thread/sleep 3000) ;; Waiting 3s before trying again.
                                                                                             (connect-to-slack conn token))))
                                                 (rtm/sub-to-event (:events-publication @conn) :message message-receiver))
                                           (connect-to-slack conn ~a)"
-					 tokenvar channelsvar channelsvar tokenvar)
+					 tokenvar tokenvar channelsvar channelsvar tokenvar)
 					`((,tokenvar ,(->jstring token))
 					  (,channelsvar ,(->jobject channels))))))
                #t)))
+
+
