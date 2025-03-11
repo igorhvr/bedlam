7e99e7561dc67ac8b9d0218e3ecc0a7cd6038ec8
Igor Hjelmstrom Vinhas Ribeiro <igorhvr@iasylum.net>
2025-03-11 02:16:22 +0000

    Update

--- iasylum/json.scm
+++ iasylum/json.scm
@@ -1,10 +1,10 @@
 (require-extension (lib iasylum/packrat))
 (require-extension (lib iasylum/math))
 (require-extension (lib iasylum/srfi-89))
-(require-extension (lib iasylum/irregex))
 (require-extension (srfi 69))
 (require-extension (srfi 23))
 (require-extension (srfi 19))
+(require-library 'iasylum/irregex)
 
 (module iasylum/json
   (json-read json-write scheme->json json->scheme beautify-json json->sxml json->sxml-block sort-json-object-by-keys add-missing-properties-to-json-object header-and-data-list->json)
